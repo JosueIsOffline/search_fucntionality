cdb4f1ce9116c2d11514b543fc9c7cd6de62ca03
Josue Ricardo Hernández Montero <73363951+JosueIsOffline@users.noreply.github.com>
2023-04-21 20:08:20 -0400

    Agregar archivos de proyecto.

---
+++ search_functionality/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace search_functionality
+{
+    internal class Conexion
+    {
+        public static string cConexion = "Data Source=LAPTOP-EHB7IG9Q;Initial Catalog=search;Integrated Security=True";
+    }
+}
---
+++ search_functionality/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace search_functionality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Document> documents = SearchDocument("Contenido 1", false);
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("Document ID: " + document.Id);
+                Console.WriteLine("Title: " + document.Title);
+                Console.WriteLine("Content: " + document.Content);
+
+            }
+
+
+            //List<Document> documents = SearchDocument("Contenido 2", true);
+
+            //foreach (Document document in documents)
+            //{
+            //    Console.WriteLine("Document ID: " + document.Id);
+            //    Console.WriteLine("Title: " + document.Title);
+            //    Console.WriteLine("Content: " + document.Content);
+            //    Console.WriteLine();
+            //}
+        }
+
+        public static List<Document> SearchDocument(string query, bool matchAll)
+        {
+            query = query.Trim().ToLower();
+
+            string[] words = query.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            string sql = "SELECT id, title, content from document where";
+
+            if (matchAll)
+            {
+                sql += string.Join(" AND ", words.Select(word => $"(LOWER(title) LIKE '%{word}%' OR LOWER(content) LIKE '%{word}%')"));
+            }
+            else
+            {
+                sql += string.Join(" OR ", words.Select(word => $"(LOWER(title) LIKE '%{word}%' OR LOWER(content) LIKE '%{word}%')"));
+            }
+
+            using (SqlConnection conn = new SqlConnection(Conexion.cConexion))
+            {
+                conn.Open();
+
+                using( SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        var documents = new List<Document>();
+                        while (reader.Read())
+                        {
+                            documents.Add(new Document
+                            {
+                                Id = reader.GetInt32(0),
+                                Title = reader.GetString(1),
+                                Content = reader.GetString(2)
+                            });
+                        }
+                        return documents;
+                    }
+                }
+            }
+        }
+    }
+}
